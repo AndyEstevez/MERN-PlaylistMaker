3781ebe68074c6dacff6ac11f0286b979650fbce
unknown <andyestevez27@gmail.com>
2021-02-02 14:20:10 -0500

    Created playlist schema

---
+++ backend/models/playlist.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema({
+    num: { type: String },
+    image: { type: String },
+    name: { type: String },
+    artist: { type: String },
+    album: { type: String },
+    duration: { type: String }
+})
+
+const playlistSchema = new Schema({
+    playlistName:{ type: String, required: true },
+    playlistDescription:{ type: String, default: 'none', maxLength: 50},
+    playlistSongs: [songSchema],
+    playlistCreator: { type: String, required: true },
+}, {
+    timestamps: true,
+});
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
+module.exports = Playlist;
