cd1c0de0ac6af30e29760ce3ed9b10f03ed0c43c
unknown <andyestevez27@gmail.com>
2021-02-01 20:46:38 -0500

    Added MongoDB Atlas connection to backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, 
+    useCreateIndex: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established");
+});
+
+
 app.listen(port, () => {
     console.log(`Server on port: ${port}`);
 });
-    
